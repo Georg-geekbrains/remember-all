b631b7207d82c336438df20853256f94610dfd92
Georg <georghome1@gmail.com>
2023-02-19 15:07:14 +0300

    change file

--- conWork/Program.cs
+++ conWork/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
 
 /* Написать программу, которая из имеющегося массива строк формирует 
